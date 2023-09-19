393ef2ff69aff3c0e45f34d2373d07d81dc19542
Alex <Alex@DESKTOP-ALEX>
2023-09-19 13:23:11 +0300

    log path

--- VeeamTestTask/FolderSync/FolderSync.cs
+++ VeeamTestTask/FolderSync/FolderSync.cs
@@ -12,21 +12,44 @@ namespace VeeamTestTask.FolderSync
         private string replicaPath;
         private string logPath;
 
-        public FolderSync(string sourcePath, string replicaPath, string logPath)
+        public FolderSync(string sourcePath, string replicaPath)
         {
             this.sourcePath = sourcePath;
             this.replicaPath = replicaPath;
-            this.logPath = logPath;
+            LogPathGenerate();
+        }
+
+        private void LogPathGenerate()
+        {
+            string desktopDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string logsFilePath = Path.Combine(desktopDirectory, @"VeeamTestTask\VeeamTestTask\Logs\logs.txt");
+            logPath = logsFilePath;
         }
 
         public void SynchronizeFolders()
         {
-            throw new NotImplementedException();
+            try
+            {
+                LogFile("Synchronization completed successfully.");
+            }
+            catch (Exception e)
+            {
+                LogFile("Error during synchronization: " + e.Message);
+            }
         }
 
         public void LogFile(string message)
         {
-            throw new NotImplementedException();
+            try
+            {
+                string entry = DateTime.Now + ": " + message;
+                Console.WriteLine(entry);
+                File.AppendAllText(logPath, entry + Environment.NewLine);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error logging: " + e.Message);
+            }
         }
     }
 }
--- VeeamTestTask/Program.cs
+++ VeeamTestTask/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using VeeamTestTask.FolderSync;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        FolderSync sync = new FolderSync(
+            "C:\\Users\\Alex\\Desktop\\VeeamTestTask\\VeeamTestTask\\DataSource\\",
+            "C:\\Users\\Alex\\Desktop\\VeeamTestTask\\VeeamTestTask\\DataReplica\\");
+
+        sync.SynchronizeFolders();
+    }
+}
