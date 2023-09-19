d6f2e857d2fd878bdda9deaee2e261269b019215
Alex <Alex@DESKTOP-ALEX>
2023-09-19 12:55:59 +0300

    FolderSync Interface and Class

---
+++ VeeamTestTask/FolderSync/FolderSync.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeeamTestTask.FolderSync
+{
+    internal class FolderSync : IFolderSync
+    {
+        private string sourcePath;
+        private string replicaPath;
+        private string logPath;
+
+        public FolderSync(string sourcePath, string replicaPath, string logPath)
+        {
+            this.sourcePath = sourcePath;
+            this.replicaPath = replicaPath;
+            this.logPath = logPath;
+        }
+
+        public void SynchronizeFolders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogFile(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VeeamTestTask/FolderSync/IFolderSync.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeeamTestTask.FolderSync
+{
+    internal interface IFolderSync
+    {
+        public void SynchronizeFolders();
+        public void LogFile(string message);
+    }
+}
